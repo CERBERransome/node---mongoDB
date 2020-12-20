eeaf34face180b6c556cdbc5071526b977987730
Chan <dauni0419@gmail.com>
2020-12-20 22:49:59 +0900

    db 내용 추가

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id:3322,
+        title:"awesome",
+        description:"wow awesome",
+        views:8282,
+        videoFile:"https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id:121212,
+            name:"chan",
+            email:"leeyu@chan.com"
+        }
+    },
+    {
+        id:2212,
+        title:"super",
+        description:"wow super",
+        views:333,
+        videoFile:"https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id:1212,
+            name:"chan",
+            email:"leeyu@chan.com"
+        }
+    },
+    {
+        id:565,
+        title:"nice",
+        description:"wow nice",
+        views:3344,
+        videoFile:"https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id:1231,
+            name:"chan",
+            email:"leeyu@chan.com"
+        }
+    },
+    {
+        id:121233,
+        title:"funny",
+        description:"wow funny",
+        views:123,
+        videoFile:"https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+            id:3456456,
+            name:"chan",
+            email:"leeyu@chan.com"
+        }
+    },
+]
