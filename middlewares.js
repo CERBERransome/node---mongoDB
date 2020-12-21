56535b2289315664d0e08ca268dd7e567214f743
Chan <dauni0419@gmail.com>
2020-12-21 19:36:40 +0900

    del 주석

--- middlewares.js
+++ middlewares.js
@@ -9,5 +9,3 @@ export const localMiddleware = (req,res,next) => {
     }
     next();
 }
-//이렇게 local.valName으로 전역변수를 선언할수있다 이러면 pug파일에서 #{}을 해서 이 siteName을 불러올수있다
-//당연히 app.use(router.home, globalRouter);하기 전에 app.use(ocalMiddleware)을 해야한다
