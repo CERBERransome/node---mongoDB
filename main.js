7c98c4e73e8783949375c61272b118c6ddb64865
Chan <dauni0419@gmail.com>
2020-12-21 19:17:46 +0900

    mut!

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-//*************** import ***************
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
@@ -9,7 +8,6 @@ import { localMiddleware } from "./middlewares";
 import router from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-//*************** import ***************
 const app = express()
 
 app.set("view engine", "pug");
@@ -24,7 +22,6 @@ app.use(localMiddleware)
 app.use(router.home, globalRouter);
 
 app.use(router.user, userRouter);
-//이렇게하면 /user/password, /user/edit, user등이 생길수 있다(use로 꼭 왜냐면 누군가가 /user에 접속하면 userRouter전체를 쓰겠단 의미다) magic
 app.use(router.video, videoRouter);
 
 
